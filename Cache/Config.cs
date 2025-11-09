5864f678f278eb7b7300c47ca5a55e57e0f21b6b
YellowMacaroni <92680766+YellowMacaroni@users.noreply.github.com>
2025-11-09 18:54:04 +0000

    Added new configs

--- Cache/Config.cs
+++ Cache/Config.cs
@@ -1,10 +1,12 @@
-﻿using Npgsql;
+﻿using Newtonsoft.Json;
+using Npgsql;
 using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Whispbot.Commands;
 using Whispbot.Databases;
 using YellowMacaroni.Discord.Cache;
 using YellowMacaroni.Discord.Extentions;
@@ -20,20 +22,57 @@ namespace Whispbot
               [long.Parse(key)]
             );
 
-            if (existingRecord is not null) return existingRecord;
-
-            return Postgres.SelectFirst<GuildConfig>(
+            return existingRecord ?? Postgres.SelectFirst<GuildConfig>(
                 @"INSERT INTO guild_config (id, name) VALUES (@1, @2) RETURNING *;",
                 [long.Parse(key), DiscordCache.Guilds.Get(key).WaitFor()?.name]
             );
         });
 
+        public static readonly Collection<ShiftConfig> ShiftConfig = new(async (key, args) =>
+        {
+            return Postgres.SelectFirst<ShiftConfig>(
+                @"SELECT * FROM module_shifts WHERE id = @1;", [long.Parse(key)]
+            ) ?? new();
+        });
+
+
+        public static readonly Collection<UserConfig> UserConfig = new(async (key, args) =>
+        {
+            UserConfig? existingRecord = Postgres.SelectFirst<UserConfig>(
+              @"SELECT * FROM user_config WHERE id = @1;",
+              [long.Parse(key)]
+            );
+
+            return existingRecord ?? Postgres.SelectFirst<UserConfig>(
+                @"INSERT INTO user_config (id) VALUES (@1) RETURNING *;",
+                [long.Parse(key)]
+            );
+        });
+
+        public static readonly Collection<List<ERLCServerConfig>> ERLCServerConfigs = new(async (key, args) =>
+        {
+            return Postgres.Select<ERLCServerConfig>(
+                @"SELECT * FROM erlc_servers WHERE guild_id = @1;",
+                [long.Parse(key)]
+            );
+        });
+
         public static readonly Collection<List<ShiftType>> ShiftTypes = new(async (key, args) =>
         {
-            return Postgres.Select<ShiftType>(
+            List<ShiftType>? types = Postgres.Select<ShiftType>(
                 @"SELECT * FROM shift_types WHERE guild_id = @1;",
                 [long.Parse(key)]
             );
+
+            if (types is not null && types.Count == 0)
+            {
+                types = [Postgres.SelectFirst<ShiftType>(
+                    @"INSERT INTO shift_types (guild_id, is_default) VALUES (@1, true) RETURNING *;",
+                    [long.Parse(key)]
+                )];
+            }
+
+            return types;
         });
     }
 
@@ -45,7 +84,33 @@ namespace Whispbot
         public BotVersion version = BotVersion.Production;
         public long enabled_modules = 0;
 
-        public long? default_shift_log_channel = null;
+        public int? default_language = 0;
+    }
+
+    public class ShiftConfig
+    {
+        public long id = 0;
+
+        public long? default_log_channel_id = null;
+    }
+
+
+    public class UserConfig
+    {
+        public long id = 0;
+        public long? roblox_id = null;
+        public DateTimeOffset created_at = DateTimeOffset.MinValue;
+        public int? language = 0;
+        public bool ack_required = false;
+    }
+
+    public class ERLCServerConfig
+    {
+        public string id = "";
+        public long guild_id = 0;
+        public string api_key = "";
+        public int ingame_players = 0;
+        public string? name = null;
     }
 
     public enum BotVersion
@@ -67,5 +132,6 @@ namespace Whispbot
         public List<string> triggers = [];
         public long? role_id = null;
         public long? log_channel_id = null;
+        public List<string>? required_roles = [];
     }
 }
