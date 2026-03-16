2fd96372b4fa509aa91f954d23b2a8bc4fdada19
YellowMacaroni <mattctaylor452@gmail.com>
2026-03-16 17:43:23 +0000

    feat: Add !reason command

---
+++ Commands/Discord Moderation/Reason.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whispbot.Tools.Discord;
+using YellowMacaroni.Discord.Core;
+using YellowMacaroni.Discord.Extentions;
+
+namespace Whispbot.Commands.Discord_Moderation
+{
+    public class Reason : Command
+    {
+        public override string Name => "Reason";
+        public override string Description => "Modify the reason of a Discord moderation";
+        public override Module Module => Module.DiscordModeration;
+        public override bool GuildOnly => false;
+        public override List<RateLimit> Ratelimits => [];
+        public override List<string> Aliases => ["reason"];
+        public override List<string> Usage => [];
+        public override async Task ExecuteAsync(CommandContext ctx)
+        {
+            if (ctx.Guild is null || ctx.UserId is null) return;
+
+            if (!await DiscordPermissions.HasPermissionOrAdmin(
+                ctx.Guild,
+                ctx.UserId,
+                Permissions.ManageGuild |
+                Permissions.BanMembers |
+                Permissions.KickMembers |
+                Permissions.ModerateMembers
+            ))
+            {
+                await ctx.Reply("{emoji.cross} {string.errors.dm.no_permission}.");
+                return;
+            }
+
+            string? caseIdArg = ctx.args.FirstOrDefault();
+            if (caseIdArg is null) 
+            {
+                await ctx.Reply("{emoji.cross} {string.errors.dm.no_case_provided}.");
+                return;
+            }
+
+            ctx.args.RemoveAt(0);
+            if (ctx.args.Count == 0)
+            {
+                await ctx.Reply("{emoji.cross} {string.errors.dm.no_reason_provided}.");
+                return;
+            }
+            string newReason = ctx.args.Join(" ");
+
+            int caseId = 0;
+            if (int.TryParse(caseIdArg, out int parsedArg))
+            {
+                caseId = parsedArg;
+            }
+            else if (caseIdArg.Equals("last", StringComparison.CurrentCultureIgnoreCase))
+            {
+                caseId = -1;
+            }
+            else if (caseIdArg.Equals("slast", StringComparison.CurrentCultureIgnoreCase))
+            {
+                caseId = -2;
+            }
+
+            if (caseId == 0)
+            {
+                await ctx.Reply("{emoji.cross} {string.errors.dm.invalid_case_id}.");
+                return;
+            }
+
+            var updatedCase = await DiscordModeration.EditReason(ctx.Guild, caseId, ctx.User!, newReason);
+
+            if (updatedCase is null)
+            {
+                await ctx.Reply("{emoji.cross} {string.errors.dm.failed_update_case}.");
+            }
+            else
+            {
+                await ctx.Reply($"{{emoji.tick}} {{string.success.dm.updated_case:id={updatedCase.case_id}}}!");
+            }
+        }
+    }
+}
---
+++ Procedures/DiscordModeration/Edit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whispbot.Databases;
+using Whispbot.Tools.Discord;
+using YellowMacaroni.Discord.Core;
+
+namespace Whispbot
+{
+    public static partial class DiscordModeration
+    {
+        public static async Task UpdateMessage(DiscordModerationCase? modifiedCase)
+        {
+            if (modifiedCase?.message_id is not null)
+            {
+                var config = await WhispCache.GuildConfig.Get(modifiedCase.guild_id.ToString());
+                if (config is null) return;
+
+                var logChannelId = config.discord_moderation?.log_channel_id;
+                if (logChannelId is not null)
+                {
+                    var logMessage = new Message
+                    { // who has time to fetch a message if you can just pretend you did
+                        id = modifiedCase.message_id.ToString()!,
+                        channel_id = logChannelId.ToString()!
+                    };
+                    await logMessage.Edit(await GenerateLogMessage(modifiedCase));
+                }
+            }
+        }
+
+        public static async Task<DiscordModerationCase?> EditReason(Guild guild, int caseId, User moderator, string newReason)
+        {
+            var canUpdateAny = await DiscordPermissions.HasPermissionOrAdmin(
+                guild, 
+                moderator.id, 
+                Permissions.ManageGuild
+            );
+
+            DiscordModerationCase? modifiedCase = null;
+            if (caseId < 0) // Edit last case, -1 = own, -2 = guild
+            {
+                modifiedCase = Postgres.SelectFirst<DiscordModerationCase>(
+                    $@"
+                    UPDATE discord_moderations
+                    SET reason = @1, updated_at = now(), updated_by = @2 
+                    WHERE case_id = (
+                        SELECT case_id FROM discord_moderations 
+                        WHERE guild_id = @3{(caseId == -1 ? " AND moderator_id = @2" : "")}
+                        ORDER BY created_at DESC
+                        LIMIT 1
+                    ) AND guild_id = @3
+                    RETURNING *;
+                    ",
+                    [newReason, long.Parse(moderator.id), long.Parse(guild.id)]
+                );
+            }
+            else if (caseId > 0)
+            {
+                modifiedCase = Postgres.SelectFirst<DiscordModerationCase>(
+                    $@"
+                    UPDATE discord_moderations 
+                    SET reason = @1, updated_at = now(), updated_by = @2 
+                    WHERE case_id = @3 AND guild_id = @4{(!canUpdateAny ? " AND moderator_id = @2" : "")}
+                    RETURNING *;
+                    ",
+                    [newReason, long.Parse(moderator.id), caseId, long.Parse(guild.id)]
+                );
+            }
+
+            await DeleteMessage(modifiedCase);
+
+            return modifiedCase;
+        }
+    }
+}
